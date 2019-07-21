7f0bb7817635287c4c3749498fe686c8359b2e7f
heishizixingk <3428499+heishizixingk@users.noreply.github.com>
2019-07-22 00:04:15 +0800

    powerline & new op

--- media/js/main.js
+++ media/js/main.js
@@ -73,3 +73,17 @@ $(document).ready(function() {
      ******************************************************************/
     prettyPrint();
 });
+
+$(function(){
+    function footerPosition(){
+        $("footer").removeClass("fixed-bottom");
+        var contentHeight = document.body.scrollHeight,//网页正文全文高度
+            winHeight = window.innerHeight;//可视窗口高度，不包括浏览器顶部工具栏
+        if(!(contentHeight > winHeight)){
+            //当网页正文高度小于可视窗口高度时，为 footer 添加类 fixed-bottom
+            $("footer").addClass("fixed-bottom");
+        }
+    }
+    footerPosition();
+    $(window).resize(footerPosition);
+});
